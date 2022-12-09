692584f0c0c8dd6bf625fa998639e819ab5aa098
Exhore <nelsonrivca@gmail.com>
2022-12-09 21:50:18 +0000

    +info insertion sort

--- insertionSort.py
+++ insertionSort.py
@@ -1,9 +1,9 @@
 def insertionSort(list=[]):
 
     """ Insertion Sort - Para pequeños sets de datos.
-    
-    
-    
+    - Si el array está ordenado previamente pero al revés, no debería de usar este algoritmo.
+    - Debe de tener un contenido heterógeno.
+
      """
 
     for i in range(1, len(list)):
@@ -16,3 +16,19 @@ def insertionSort(list=[]):
         list[j + 1] = key
 
     return list
+
+# versión alternativa
+
+"""
+def insertionSortAlt(list=[]):
+
+    number = len(list)
+    for i in range(1, number):
+        x = number[i]
+        j = i
+        while j >= 1 and number[j - 1] > number:
+            number[j] = number[j - 1]
+            j -= 1
+        number[j] = x
+    return number """
+
